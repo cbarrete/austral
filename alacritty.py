46b1477b5aa51152056eef17e538792e487a477d
Cédric Barreteau <cbarrete@users.noreply.github.com>
2021-04-03 14:50:10 +0200

    Add alacritty support

---
+++ alacritty.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+import yaml
+
+from aurora import palette
+
+def get_current_config(path):
+    path = Path(path)
+    if not path.is_file():
+        return {}
+    with open(path, 'r') as file:
+        return yaml.load(file, Loader=yaml.FullLoader)
+
+def make_colors_config(palette):
+    return {
+        'primary': {
+            'foreground': palette['fg'],
+            'background': palette['bg'],
+        },
+        'normal': {
+            'black':   palette['bg'],
+            'red':     palette['red'],
+            'green':   palette['green'],
+            'yellow':  palette['dark_yellow'],
+            'blue':    palette['blue'],
+            'magenta': palette['purple'],
+            'cyan':    palette['cyan'],
+            'white':   palette['white'],
+        },
+        'bright': {
+            'black':   palette['line_grey'],
+            'red':     palette['error_red'],
+            'green':   palette['light_green'],
+            'yellow':  palette['yellow'],
+            'blue':    palette['light_blue'],
+            'magenta': palette['purple'],
+            'cyan':    palette['vivid_blue'],
+            'white':   palette['fg'],
+        },
+        'cursor': {
+            'text': 'CellBackground',
+            'cursor': palette['green'],
+        },
+        'vi_mode_cursor': {
+            'text': 'CellBackground',
+            'cursor': palette['green'],
+        },
+        'search': {
+            'matches': {
+                'foreground': palette['dark_yellow'],
+                'background': palette['bg'],
+            },
+            'focused_match': {
+                'foreground': 'CellBackground',
+                'background': 'CellForeground',
+            },
+            'bar': {
+                'foreground': palette['bg'],
+                'background': palette['dark_yellow'],
+            },
+        },
+        'line_indicator': {
+            'foreground': palette['bg'],
+            'background': palette['dark_yellow'],
+        },
+    }
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from pathlib import Path
+import yaml
+
+import alacritty
 import zathura
 
 from aurora import palette
 
-with open(str(Path.home()) + '/.config/zathura/colors', 'w') as file:
+home = str(Path.home())
+with open(home + '/.config/zathura/colors', 'w') as file:
     for line in zathura.generate(palette):
         file.write(line)
+
+alacritty_path = home + '/.config/alacritty/alacritty.yml'
+alacritty_config = alacritty.get_current_config(alacritty_path)
+alacritty_config['colors'] = alacritty.make_colors_config(palette)
+with open(alacritty_path, 'w') as file:
+    yaml.dump(alacritty_config, file)
